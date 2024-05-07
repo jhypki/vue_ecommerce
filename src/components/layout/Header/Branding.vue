2beea13faa968db090ff7d862597406186b90029
Jakub Hypki <kubahypki7@gmail.com>
2024-05-07 11:41:35 +0200

    chore: Update tsconfig.json and package.json
    The `tsconfig.json` file was modified to change the `strict` flag from `true` to `false`. This change was made to relax the strict type checking in the TypeScript compiler.
    
    The `package.json` file was modified to add new dependencies: `@vueuse/core`, `oh-vue-icons`, `vitest`, and `vue-router`. Additionally, the dev dependencies `@iconify-json/radix-icons` and `@iconify/vue` were added.
    
    These changes were made to update the project dependencies and improve the development environment.

---
+++ src/components/layout/Header/Branding.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+
+</script>
+<template>
+    <div>
+        <span class="text-lg font-bold text-primary">Shopper</span>
+    </div>    
+</template>
+
+
+
+<style scoped>
+</style>
---
+++ src/components/layout/Header/Header.vue
@@ -0,0 +1,17 @@
+<template >
+    <div class='w-full min-h-16 bg-background border-b flex items-center justify-between px-4'>
+        <Branding/>
+        <Button>button</Button>
+        <ToggleThemeButton />
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import ToggleThemeButton from './ToggleThemeButton.vue'
+import Branding from './Branding.vue'
+</script>
+<style lang="">
+    
+</style>
